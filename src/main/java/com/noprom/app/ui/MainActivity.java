fdcd8218584213a8d3689913d02c3dd37c3ea165
noprom <874702417@qq.com>
2015-02-23 22:46:44 +0800

    change the os of mainActicity
    change the os of mainActicity

--- src/main/java/com/noprom/app/ui/MainActivity.java
+++ src/main/java/com/noprom/app/ui/MainActivity.java
@@ -1,113 +1,134 @@
+/*
+ * Copyright 2015 Rudson Lima
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.noprom.app.ui;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.Gravity;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.util.SparseIntArray;
+import android.view.Menu;
 import android.view.View;
-import android.widget.AdapterView;
 import android.widget.Toast;
 
-import com.mikepenz.iconics.typeface.FontAwesome;
-import com.mikepenz.materialdrawer.Drawer;
-import com.mikepenz.materialdrawer.model.DividerDrawerItem;
-import com.mikepenz.materialdrawer.model.PrimaryDrawerItem;
-import com.mikepenz.materialdrawer.model.SecondaryDrawerItem;
-import com.mikepenz.materialdrawer.model.SectionDrawerItem;
-import com.mikepenz.materialdrawer.model.interfaces.Badgeable;
-import com.mikepenz.materialdrawer.model.interfaces.IDrawerItem;
-import com.mikepenz.materialdrawer.model.interfaces.Nameable;
 import com.noprom.app.R;
 
-public class MainActivity extends ActionBarActivity {
+import java.util.ArrayList;
+import java.util.List;
 
-    private Drawer.Result result = null;
+import br.liveo.interfaces.NavigationLiveoListener;
+import br.liveo.navigationliveo.NavigationLiveo;
+
+public class MainActivity extends NavigationLiveo implements NavigationLiveoListener {
+
+    public List<String> mListNameItem;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        //supportRequestWindowFeature(Window.FEATURE_INDETERMINATE_PROGRESS);
+    public void onUserInformation() {
+        //User information here
+        this.mUserName.setText("Rudson Lima");
+        this.mUserEmail.setText("rudsonlive@gmail.com");
+        this.mUserPhoto.setImageResource(R.drawable.ic_rudsonlive);
+        this.mUserBackground.setImageResource(R.drawable.ic_user_background);
+    }
 
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+    @Override
+    public void onInt(Bundle savedInstanceState) {
+        //Creation of the list items is here
 
-        // Handle Toolbar
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        // set listener {required}
+        this.setNavigationListener(this);
 
-        result = new Drawer()
-                .withActivity(this)
-                .withToolbar(toolbar)
-                .withActionBarDrawerToggle(true)
-                .withHeader(R.layout.header)
-                .addDrawerItems(
-                        new PrimaryDrawerItem().withName(R.string.drawer_item_home).withIcon(FontAwesome.Icon.faw_home).withBadge("99").withIdentifier(1),
-                        new PrimaryDrawerItem().withName(R.string.drawer_item_free_play).withIcon(FontAwesome.Icon.faw_gamepad),
-                        new PrimaryDrawerItem().withName(R.string.drawer_item_custom).withIcon(FontAwesome.Icon.faw_eye).withBadge("6").withIdentifier(2),
-                        new SectionDrawerItem().withName(R.string.drawer_item_section_header),
-                        new SecondaryDrawerItem().withName(R.string.drawer_item_settings).withIcon(FontAwesome.Icon.faw_cog),
-                        new SecondaryDrawerItem().withName(R.string.drawer_item_help).withIcon(FontAwesome.Icon.faw_question).setEnabled(false),
-                        new SecondaryDrawerItem().withName(R.string.drawer_item_open_source).withIcon(FontAwesome.Icon.faw_github).withBadge("12").withIdentifier(1),
-                        new SecondaryDrawerItem().withName(R.string.drawer_item_contact).withIcon(FontAwesome.Icon.faw_bullhorn)
-                )
-                .withOnDrawerListener(new Drawer.OnDrawerListener() {
-                    @Override
-                    public void onDrawerOpened(View drawerView) {
-                        Toast.makeText(MainActivity.this, "onDrawerOpened", Toast.LENGTH_SHORT).show();
-                    }
+        //First item of the position selected from the list
+        this.setDefaultStartPositionNavigation(1);
 
-                    @Override
-                    public void onDrawerClosed(View drawerView) {
-                        Toast.makeText(MainActivity.this, "onDrawerClosed", Toast.LENGTH_SHORT).show();
-                    }
-                })
-                .withOnDrawerItemClickListener(new Drawer.OnDrawerItemClickListener() {
-                    @Override
-                    public void onItemClick(AdapterView<?> parent, View view, int position, long id, IDrawerItem drawerItem) {
-                        if (drawerItem instanceof Nameable) {
-                            Toast.makeText(MainActivity.this, MainActivity.this.getString(((Nameable) drawerItem).getNameRes()), Toast.LENGTH_SHORT).show();
-                        }
+        // name of the list items
+        mListNameItem = new ArrayList<>();
+        mListNameItem.add(0, getString(R.string.inbox));
+        mListNameItem.add(1, getString(R.string.starred));
+        mListNameItem.add(2, getString(R.string.sent_mail));
+        mListNameItem.add(3, getString(R.string.drafts));
+        mListNameItem.add(4, getString(R.string.more_markers)); //This item will be a subHeader
+        mListNameItem.add(5, getString(R.string.trash));
+        mListNameItem.add(6, getString(R.string.spam));
 
-                        if (drawerItem instanceof Badgeable) {
-                            Badgeable badgeable = (Badgeable) drawerItem;
-                            if (badgeable.getBadge() != null) {
-                                //note don't do this if your badge contains a "+"
-                                int badge = Integer.valueOf(badgeable.getBadge());
-                                if (badge > 0) {
-                                    result.updateBadge(String.valueOf(badge - 1), position);
-                                }
-                            }
-                        }
-                    }
-                })
-                .withOnDrawerItemLongClickListener(new Drawer.OnDrawerItemLongClickListener() {
-                    @Override
-                    public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id, IDrawerItem drawerItem) {
-                        if (drawerItem instanceof SecondaryDrawerItem) {
-                            Toast.makeText(MainActivity.this, MainActivity.this.getString(((SecondaryDrawerItem) drawerItem).getNameRes()), Toast.LENGTH_SHORT).show();
-                        }
-                        return false;
-                    }
-                })
-                .build();
+        // icons list items
+        List<Integer> mListIconItem = new ArrayList<>();
+        mListIconItem.add(0, R.drawable.ic_inbox_black_24dp);
+        mListIconItem.add(1, R.drawable.ic_star_black_24dp); //Item no icon set 0
+        mListIconItem.add(2, R.drawable.ic_send_black_24dp); //Item no icon set 0
+        mListIconItem.add(3, R.drawable.ic_drafts_black_24dp);
+        mListIconItem.add(4, 0); //When the item is a subHeader the value of the icon 0
+        mListIconItem.add(5, R.drawable.ic_delete_black_24dp);
+        mListIconItem.add(6, R.drawable.ic_report_black_24dp);
 
-        new Drawer()
-                .withActivity(this)
-                .withFooter(R.layout.footer)
-                .withStickyFooter(R.layout.footer)
-                .addDrawerItems(
-                        new PrimaryDrawerItem().withName(R.string.drawer_item_custom).withIcon(FontAwesome.Icon.faw_eye),
-                        new PrimaryDrawerItem().withName(R.string.drawer_item_home).withIcon(FontAwesome.Icon.faw_home),
-                        new PrimaryDrawerItem().withName(R.string.drawer_item_free_play).withIcon(FontAwesome.Icon.faw_gamepad),
-                        new SectionDrawerItem().withName(R.string.drawer_item_section_header),
-                        new SecondaryDrawerItem().withName(R.string.drawer_item_settings).withIcon(FontAwesome.Icon.faw_cog),
-                        new DividerDrawerItem(),
-                        new SecondaryDrawerItem().withName(R.string.drawer_item_open_source).withIcon(FontAwesome.Icon.faw_github),
-                        new SecondaryDrawerItem().withName(R.string.drawer_item_help).withIcon(FontAwesome.Icon.faw_question).setEnabled(false),
-                        new SectionDrawerItem().withName(R.string.drawer_item_section_header),
-                        new SecondaryDrawerItem().withName(R.string.drawer_item_contact).withIcon(FontAwesome.Icon.faw_bullhorn)
-                )
-                .withDrawerGravity(Gravity.END)
-                .append(result);
+        //{optional} - Among the names there is some subheader, you must indicate it here
+        List<Integer> mListHeaderItem = new ArrayList<>();
+        mListHeaderItem.add(4);
+
+        //{optional} - Among the names there is any item counter, you must indicate it (position) and the value here
+        SparseIntArray mSparseCounterItem = new SparseIntArray(); //indicate all items that have a counter
+        mSparseCounterItem.put(0, 7);
+        mSparseCounterItem.put(1, 123);
+        mSparseCounterItem.put(6, 250);
+
+        //If not please use the FooterDrawer use the setFooterVisible(boolean visible) method with value false
+        this.setFooterInformationDrawer(R.string.settings, R.drawable.ic_settings_black_24dp);
+
+        this.setNavigationAdapter(mListNameItem, mListIconItem, mListHeaderItem, mSparseCounterItem);
+    }
+
+    @Override
+    public void onItemClickNavigation(int position, int layoutContainerId) {
+
+        FragmentManager mFragmentManager = getSupportFragmentManager();
+
+        Fragment mFragment = new FragmentMain().newInstance(mListNameItem.get(position));
+
+        if (mFragment != null){
+            mFragmentManager.beginTransaction().replace(layoutContainerId, mFragment).commit();
+        }
+    }
+
+    @Override
+    public void onPrepareOptionsMenuNavigation(Menu menu, int position, boolean visible) {
+
+        //hide the menu when the navigation is opens
+        switch (position) {
+            case 0:
+                menu.findItem(R.id.menu_add).setVisible(!visible);
+                menu.findItem(R.id.menu_search).setVisible(!visible);
+                break;
+
+            case 1:
+                menu.findItem(R.id.menu_add).setVisible(!visible);
+                menu.findItem(R.id.menu_search).setVisible(!visible);
+                break;
+        }
+    }
+
+    @Override
+    public void onClickUserPhotoNavigation(View v) {
+        //user photo onClick
+        Toast.makeText(this, R.string.open_user_profile, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onClickFooterItemNavigation(View v) {
+        //footer onClick
+        startActivity(new Intent(this, SettingsActivity.class));
     }
 }
