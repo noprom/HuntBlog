1a5d2a69190d001b8d65e049060d6c32c1a7bc26
noprom <874702417@qq.com>
2015-02-25 16:19:19 +0800

    新增动画以及 小图片
    新增动画以及 小图片

--- src/main/java/com/noprom/app/AppContext.java
+++ src/main/java/com/noprom/app/AppContext.java
@@ -5,6 +5,8 @@ import android.app.Application;
 /**
  * 全局应用程序类：用于保存和调用全局应用配置及访问网络数据
  *
+ * @author noprom (http://github.com/noprom)
+ * @version 1.0
  * Created by noprom on 2015/2/22.
  */
 public class AppContext extends Application {
