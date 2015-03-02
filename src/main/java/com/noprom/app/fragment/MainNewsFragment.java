0b90a307fd0158dade5be7fb79fc159d63d10f4e
noprom <874702417@qq.com>
2015-03-02 23:02:58 +0800

    【处理】handleLvData 数据处理
    handleLvData 数据处理

--- src/main/java/com/noprom/app/fragment/MainNewsFragment.java
+++ src/main/java/com/noprom/app/fragment/MainNewsFragment.java
@@ -1,14 +1,23 @@
 package com.noprom.app.fragment;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import com.noprom.app.R;
 import com.noprom.app.adapter.ListViewNewsAdapter;
 import com.noprom.app.bean.News;
+import com.noprom.app.bean.Notice;
+import com.noprom.app.widget.PullToRefreshListView;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -23,25 +32,105 @@ import java.util.List;
 public class MainNewsFragment extends Fragment {
 
     private View lvNews_footer;
+    private TextView lvNews_foot_more;
+    private ProgressBar lvNews_foot_progress;
+    private ListView lvNews_root;
 
+    private PullToRefreshListView lvNews;
     private ListViewNewsAdapter lvNewsAdapter;
     private List<News> lvNewsData = new ArrayList<News>();
+
+    private Handler lvNewsHandler;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        this.initNewsListView(inflater);
+        lvNews_root = (ListView) inflater.inflate(R.layout.fragment_main_news,container,false);
+        lvNewsAdapter = new ListViewNewsAdapter(getActivity(),lvNewsData,R.layout.news_listitem);
+        lvNews_footer = inflater.inflate(R.layout.listview_footer, null);
+        lvNews_foot_more = (TextView) lvNews_footer.findViewById(R.id.listview_foot_more);
+        lvNews_foot_progress = (ProgressBar) lvNews_footer.findViewById(R.id.listview_foot_progress);
+        lvNews = (PullToRefreshListView) lvNews_root.findViewById(R.id.listview_news);
+        lvNews.addFooterView(lvNews_footer);    // 添加底部视图必须在setAdapter之间
+        lvNews.setAdapter(lvNewsAdapter);
 
-        return inflater.inflate(R.layout.fragment_main_news, container, false);
+        // 设置每个选项的点击事件
+        lvNews.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                // 点击头部、底部栏目无效
+                if(position == 0 || view ==lvNews_footer)
+                    return;
+
+                News news = null;
+                if(view instanceof TextView){
+                    news = (News) view.getTag();
+                }else{
+                    TextView tv = (TextView) view.findViewById(R.id.news_listitem_title);
+                    news = (News) tv.getTag();
+                }
+                if(news == null)
+                    return;
+
+                // TODO 跳转到新闻详情页面
+
+            }
+        });
+
+        // TODO 设置下滑滚动事件
+
+
+        // 加载ListView数据
+        this.initListViewNewsData();
+
+        return lvNews_root;
+    }
+
+
+    /**
+     * 加载ListView数据
+     */
+    private void initListViewNewsData() {
+        // 初始化handler
+
     }
 
     /**
-     * 初始化新闻资讯列表
+     * 获取listview的初始化Handler
+     * @param lv
+     * @param adapter
+     * @param more
+     * @param progress
+     * @param pageSize
+     * @return
      */
-    public void initNewsListView(LayoutInflater inflater){
-        lvNewsAdapter = new ListViewNewsAdapter(getActivity(),lvNewsData,R.layout.news_listitem);
-        lvNews_footer = inflater.inflate(R.layout.list)
+    private Handler getLvHandler(final PullToRefreshListView lv,
+                                 final BaseAdapter adapter,final TextView more,
+                                 final ProgressBar progress,final int pageSize){
 
+        return new Handler(){
+            @Override
+            public void handleMessage(Message msg) {
+//                super.handleMessage(msg);
+
+            }
+        };
     }
+
+    /**
+     * listview数据处理
+     * @param what
+     * @param obj
+     * @param objtype
+     * @param actiontype
+     * @return
+     */
+    private Notice handleLvData(int what,Object obj,int objtype,int actiontype){
+        Notice notice = null;
+        switch (actiontype){
+
+        }
+    }
+
 
 }
