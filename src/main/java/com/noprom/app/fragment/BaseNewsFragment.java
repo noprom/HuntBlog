71604fb09539cf03f4c7b43f181fb026c3c2d7c7
noprom <874702417@qq.com>
2015-03-05 21:17:49 +0800

    【新增】ListViewBaseFragment
    【新增】ListViewBaseFragment

---
+++ src/main/java/com/noprom/app/fragment/BaseNewsFragment.java
@@ -0,0 +1,15 @@
+package com.noprom.app.fragment;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * 新闻资讯 Fragment基类
+ *
+ * @author noprom (http://github.com/noprom)
+ * @version 1.0
+ *          Created by noprom on 2015/3/5.
+ */
+public class BaseNewsFragment extends Fragment{
+    private final String TAG = "BaseNewsFragment";
+
+}
