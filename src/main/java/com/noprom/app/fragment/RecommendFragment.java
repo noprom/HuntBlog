3d98fb5b29c1f7b993f81de22666b9fa5923b6b8
noprom <874702417@qq.com>
2015-03-06 21:55:35 +0800

    Revert "冲突"
    This reverts commit 9e963c19c88f8be8fbc7b1a150550f37276c740e.

--- src/main/java/com/noprom/app/fragment/RecommendFragment.java
+++ src/main/java/com/noprom/app/fragment/RecommendFragment.java
@@ -2,6 +2,7 @@ package com.noprom.app.fragment;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -21,6 +22,8 @@ public class RecommendFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        Log.d(TAG, "onCreateView");
+
         return inflater.inflate(R.layout.fragment_recommend, container, false);
     }
 
