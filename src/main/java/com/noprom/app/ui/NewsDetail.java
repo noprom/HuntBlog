5897cfb8f1c2fb83e1cad150fa9c1ddd1d916f95
noprom <874702417@qq.com>
2015-03-06 23:33:28 +0800

    【修改详情页底部布局】
    【修改详情页底部布局】

--- src/main/java/com/noprom/app/ui/NewsDetail.java
+++ src/main/java/com/noprom/app/ui/NewsDetail.java
@@ -5,9 +5,11 @@ import android.os.Handler;
 import android.support.v7.app.ActionBarActivity;
 import android.view.MenuItem;
 import android.webkit.WebView;
+import android.widget.TextView;
 
 import com.noprom.app.AppConfig;
 import com.noprom.app.R;
+import com.noprom.app.bean.CommentList;
 import com.noprom.app.bean.News;
 
 
@@ -16,7 +18,7 @@ import com.noprom.app.bean.News;
  *
  * @author noprom (http://github.com/noprom)
  * @version 1.0
- * Created by noprom on 2015/2/25.
+ *          Created by noprom on 2015/2/25.
  */
 public class NewsDetail extends ActionBarActivity {
 
@@ -25,8 +27,16 @@ public class NewsDetail extends ActionBarActivity {
     private Handler mHandler;
     private News newsDetail;
 
+    private TextView mTitle;
+    private TextView mAuthor;
+    private TextView mPubDate;
+    private TextView mCommentCount;
+
+
 
     private String tempCommentKey = AppConfig.TEMP_COMMENT;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,10 +55,17 @@ public class NewsDetail extends ActionBarActivity {
      * 初始化视图控件
      */
     private void initView() {
-        newsId = getIntent().getIntExtra("news_id",0);
-        if(newsId > 0){
-            tempCommentKey = AppConfig.TEMP_COMMENT + "_"+CommentL
+        newsId = getIntent().getIntExtra("news_id", 0);
+        if (newsId > 0) {
+            tempCommentKey = AppConfig.TEMP_COMMENT + "_" + CommentList.CATALOG_NEWS + "_" + newsId;
         }
+
+        mTitle = (TextView) findViewById(R.id.news_detail_title);
+        mAuthor = (TextView) findViewById(R.id.news_detail_author);
+        mPubDate = (TextView) findViewById(R.id.news_detail_date);
+        mCommentCount = (TextView) findViewById(R.id.news_detail_commentcount);
+
+
     }
 
 
