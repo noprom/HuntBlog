6d2ae467f94599fad44a06bd815b0330c78ab0cb
noprom <874702417@qq.com>
2015-03-06 22:48:55 +0800

    【新增】评论列表实体类
    【新增】评论列表实体类

--- src/main/java/com/noprom/app/ui/NewsDetail.java
+++ src/main/java/com/noprom/app/ui/NewsDetail.java
@@ -1,10 +1,14 @@
 package com.noprom.app.ui;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v7.app.ActionBarActivity;
 import android.view.MenuItem;
+import android.webkit.WebView;
 
+import com.noprom.app.AppConfig;
 import com.noprom.app.R;
+import com.noprom.app.bean.News;
 
 
 /**
@@ -16,18 +20,36 @@ import com.noprom.app.R;
  */
 public class NewsDetail extends ActionBarActivity {
 
+    private int newsId;
+    private WebView mWebView;
+    private Handler mHandler;
+    private News newsDetail;
+
+
+    private String tempCommentKey = AppConfig.TEMP_COMMENT;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_news_detail);
-
         // 允许左上方有一个返回的按钮，并且可以点击
         getSupportActionBar().setHomeButtonEnabled(true);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
+        this.initView();
+
+
     }
 
-
+    /**
+     * 初始化视图控件
+     */
+    private void initView() {
+        newsId = getIntent().getIntExtra("news_id",0);
+        if(newsId > 0){
+            tempCommentKey = AppConfig.TEMP_COMMENT + "_"+CommentL
+        }
+    }
 
 
     @Override
